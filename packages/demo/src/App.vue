dc71872d2885d1a17ee5fc8c071bdc9ca2961673
Liu Rongtao <liurongtao@patsnap.com>
2025-03-07 23:28:14 +0800

    feat(@ruxuwu/example): 修改页面样式

--- packages/demo/src/App.vue
+++ packages/demo/src/App.vue
@@ -16,13 +16,15 @@
   </div>
 </template>
 
-<style scoped>
+<style>
 .app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: #f5f7fa;
+  min-height: 100vh;
 }
 
 nav {
