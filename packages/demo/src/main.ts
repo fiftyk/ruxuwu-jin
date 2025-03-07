921c9d4dd053dc6230e405687c8a2fc7fe6eb173
Liu Rongtao <liurongtao@patsnap.com>
2025-03-07 21:22:38 +0800

    feat: @ruxuwu/example 依赖 @ruxuwu/jin

--- packages/demo/src/main.ts
+++ packages/demo/src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { pluginManager } from '@ruxuwu/jin';
 
 createApp(App).mount('#app')
+
+console.log(pluginManager)
