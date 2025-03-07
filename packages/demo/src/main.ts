9c824f94b1171a881c2a4aff75859be1b52fdbc3
Liu Rongtao <liurongtao@patsnap.com>
2025-03-08 00:43:29 +0800

    feat(@ruxuwu/example): 测试从 url 加载插件

--- packages/demo/src/main.ts
+++ packages/demo/src/main.ts
@@ -4,26 +4,17 @@ import App from './App.vue'
 import router from './router'
 import { pluginManager } from '@ruxuwu/jin';
 import { routerRegister } from './services/router-register';
-import examplePlugin from './plugins/example-plugin';
-import helloWorldPlugin from '@ruxuwu-plugins/hello-world-plugin';
 
 pluginManager.setPluginContext({ routerRegister })
-
-await pluginManager.registerPlugin({
-    id: 'example-plugin',
-    name: 'Example Plugin',
-    version: '0.1.0',
-    url: '', // 插件的 URL，如果是本地插件可以留空
-}, async () => examplePlugin);
-
-await pluginManager.activatePlugin('example-plugin');
 
 await pluginManager.registerPlugin({
     id: 'hello-world-plugin',
     name: 'Hello World Plugin',
     version: '0.1.0',
-    url: '', // 插件的 URL，如果是本地插件可以留空
-}, async () => helloWorldPlugin);
+    url: 'http://localhost:3000/index.js', // 指向 HTTP 服务器提供的插件 JS 文件
+}, (manifest) => {
+    return pluginManager.loadPlugin(manifest.url);
+});
 
 await pluginManager.activatePlugin('hello-world-plugin');
 
